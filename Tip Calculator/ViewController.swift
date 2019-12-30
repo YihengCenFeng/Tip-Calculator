12e958a34c4039052c18cd082c8da843ece24958
Yiheng Cen Feng <yihengcen@gmail.com>
2019-12-29 23:47:05 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Yiheng Cen Feng on 12/29/19.
+//  Copyright © 2019 Yiheng Cen Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
